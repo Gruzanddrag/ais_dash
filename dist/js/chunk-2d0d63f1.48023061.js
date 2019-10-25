0cf56d998abc78ec4c93a0a95d43fc2937a1906b
Nikita Bulakhov <bulahov26@gmail.com>
2019-10-25 12:49:01 +0300

    init

---
+++ dist/js/chunk-2d0d63f1.48023061.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d63f1"],{7277:function(n,t,e){"use strict";e.r(t);var a=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("v-container",{attrs:{fluid:""}},[e("v-row")],1)},r=[],o={},u=o,c=e("2877"),l=e("6544"),i=e.n(l),s=e("a523"),w=e("0fd9"),d=Object(c["a"])(u,a,r,!1,null,null,null);t["default"]=d.exports;i()(d,{VContainer:s["a"],VRow:w["a"]})}}]);
+//# sourceMappingURL=chunk-2d0d63f1.48023061.js.map
